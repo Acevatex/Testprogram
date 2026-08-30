97e0aaf0eb402c789198169d5278204a3140c0e3
Ace <111625044+Acevatex@users.noreply.github.com>
2026-08-31 08:33:10 +1200

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,6 +2,3 @@
 set "WEBHOOK_URL=https://discord.com/api/webhooks/1543428498956361788/k8Vvn4dJr6ABJagHkda3ICXJctRUuG0V_X9wWENSOODf5n_AI7sp9u_AOgxlsnZdgGHE"
 
 powershell -NoProfile -Command "$body = @{content='Hello from a batch file!'} | ConvertTo-Json; Invoke-RestMethod -Uri '%WEBHOOK_URL%' -Method Post -ContentType 'application/json' -Body $body"
-
-echo Message sent.
-pause
